092087d96719a8c526a5c261930e4ba37eb8d05d
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-08 11:38:45 +0200

    fix: fixed branches to checkout for different environments

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ if ('BRANCH_NAME' in process.env == false || process.env.BRANCH_NAME == 'develop
   new PipelineStackAcceptance(app, 'irma-issue-pipeline-acceptance',
     {
       env: deploymentEnvironment,
-      branchName: 'development',
+      branchName: 'acceptance',
       deployToEnvironment: acceptanceEnvironment,
       defaultsEnvFile: 'acceptance',
       enableIrmaAuthentication: true,
@@ -51,7 +51,7 @@ if ('BRANCH_NAME' in process.env == false || process.env.BRANCH_NAME == 'develop
   new PipelineStackProduction(app, 'irma-issue-pipeline-production',
     {
       env: deploymentEnvironment,
-      branchName: 'development',
+      branchName: 'production',
       deployToEnvironment: productionEnvironment,
       defaultsEnvFile: 'production',
       enableIrmaAuthentication: false, // Never enable in production!
