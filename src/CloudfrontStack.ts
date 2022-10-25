ddbd60fb46587a05ce309cd05f0c5565886d3fd2
Marnix Dessing <m.dessing@nijmegen.nl>
2022-10-25 12:42:36 +0200

    feat: enable waf with cf update issue.js

--- src/CloudfrontStack.ts
+++ src/CloudfrontStack.ts
@@ -87,14 +87,14 @@ export class CloudfrontStack extends Stack {
    * Get the certificate ARN from parameter store in us-east-1
    * @returns string Certificate ARN
    */
-  // private wafAclId() { // TODO couple CF en WAF
-  //   const parameters = new RemoteParameters(this, 'waf-params', {
-  //     path: `${Statics.wafPath}/`,
-  //     region: 'us-east-1',
-  //   });
-  //   const wafAclId = parameters.get(Statics.ssmWafAclArn);
-  //   return wafAclId;
-  // }
+  private wafAclId() {
+    const parameters = new RemoteParameters(this, 'waf-params', {
+      path: `${Statics.wafPath}/`,
+      region: 'us-east-1',
+    });
+    const wafAclId = parameters.get(Statics.ssmWafAclArn);
+    return wafAclId;
+  }
 
   /**
    * Add static contents to cloudfront
@@ -132,14 +132,14 @@ export class CloudfrontStack extends Stack {
    */
   setCloudfrontStack(apiGatewayDomain: string, domainNames?: string[], certificateArn?: string): Distribution {
     const certificate = (certificateArn) ? CertificateManager.Certificate.fromCertificateArn(this, 'certificate', certificateArn) : undefined;
-    //const webAclId = this.wafAclId();
+    const webAclId = this.wafAclId();
     if (!certificate) { domainNames = undefined; };
 
     const distribution = new Distribution(this, 'cf-distribution', {
       priceClass: PriceClass.PRICE_CLASS_100,
       domainNames,
       certificate,
-      //webAclId,
+      webAclId,
       defaultBehavior: {
         origin: new HttpOrigin(apiGatewayDomain),
         originRequestPolicy: new OriginRequestPolicy(this, 'cf-originrequestpolicy', {
--- src/app/static-resources/static/scripts/issue.js
+++ src/app/static-resources/static/scripts/issue.js
@@ -1,31 +1,29 @@
-document.onload = () => {
-    const sessionPtrU = document.getElementById("session-ptr-u").getAttribute('data');
-    const sessionPtrQr = document.getElementById("session-ptr-qr").getAttribute('data');
-    const irmaServer = document.getElementById("irmaServer").getAttribute('data');
+const sessionPtrU = document.getElementById("session-ptr-u").getAttribute('data');
+const sessionPtrQr = document.getElementById("session-ptr-qr").getAttribute('data');
+const irmaServer = document.getElementById("irmaServer").getAttribute('data');
 
-    const irmaClient = irma.newWeb({
-        debugging: true,
-        element: '#irma-web-form',
-        session: {
-            url: irmaServer,
-            start: false, // No need to start session from the browser (done server-sied)
-            mapping: {
-                sessionPtr: r => { return { "u": sessionPtrU, "irmaqr": sessionPtrQr } },
-            },
-            result: false, // No need to fetch session result (status success / failed is sufficient)
+const irmaClient = irma.newWeb({
+    debugging: true,
+    element: '#irma-web-form',
+    session: {
+        url: irmaServer,
+        start: false, // No need to start session from the browser (done server-sied)
+        mapping: {
+            sessionPtr: r => { return { "u": sessionPtrU, "irmaqr": sessionPtrQr } },
         },
-        state: {
-            serverSentEvents: false,
-        }
-    });
+        result: false, // No need to fetch session result (status success / failed is sufficient)
+    },
+    state: {
+        serverSentEvents: false,
+    }
+});
 
-    irmaClient.start() 
-        .then(() => { // Hide QR show success message
-            document.getElementById('irma-form').classList.add("hidden");
-            document.getElementById('success').classList.remove("hidden");
-        })
-        .catch(() => { // Hide QR show error message
-            document.getElementById('irma-form').classList.add("hidden");
-            document.getElementById('failed-irma').classList.remove("hidden");
-        });
-}
+irmaClient.start()
+    .then(() => { // Hide QR show success message
+        document.getElementById('irma-form').classList.add("hidden");
+        document.getElementById('success').classList.remove("hidden");
+    })
+    .catch(() => { // Hide QR show error message
+        document.getElementById('irma-form').classList.add("hidden");
+        document.getElementById('failed-irma').classList.remove("hidden");
+    });
