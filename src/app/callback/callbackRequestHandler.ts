efb267c4979f556540c601331842fe3cd4be711f
Marnix Dessing <m.dessing@nijmegen.nl>
2022-11-24 10:56:32 +0100

    fix: do not store callback in dynamodb

--- src/app/callback/callbackRequestHandler.ts
+++ src/app/callback/callbackRequestHandler.ts
@@ -1,5 +1,5 @@
 import * as crypto from 'crypto';
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { Response } from '@gemeentenijmegen/apigateway-http';
 import { Session } from '@gemeentenijmegen/session';
 
@@ -10,7 +10,7 @@ export async function callbackRequestHandler(params: any, dynamoDBClient: Dynamo
   let session = new Session(params.cookies, dynamoDBClient);
   await session.init();
   if (session.isLoggedIn() == true) {
-    return handleLoggedinRequest(session, params, dynamoDBClient);
+    return handleLoggedinRequest(session, params);
   }
   return Response.error(403);
 }
@@ -18,12 +18,11 @@ export async function callbackRequestHandler(params: any, dynamoDBClient: Dynamo
 /**
  * Request persoonsgegevens form BRP send them to the IRMA server
  * logs the issue event and passes the irma sessionPtr to the render.
- * @param session
- * @param dynamoDBClient
+ * @param session the session object
+ * @param params the http request parameters
  * @returns
  */
-async function handleLoggedinRequest(session: Session, params: any, dynamoDBClient: DynamoDBClient) {
-
+async function handleLoggedinRequest(session: Session, params: any) {
   if (session.getValue('issued', 'BOOL')) {
     return Response.json({ message: 'Callback already performed' });
   }
@@ -31,36 +30,29 @@ async function handleLoggedinRequest(session: Session, params: any, dynamoDBClie
   const bsn = session.getValue('bsn', 'S');
   const gemeente = session.getValue('gemeente', 'S');
   const timestamp = Date.now();
-  const ttl = new Date().setFullYear(new Date().getFullYear() + 1).toString();
   const success = params.result == 'success';
   const error = params.error;
 
   // Log the issue event
-  await registerIssueEvent(dynamoDBClient, bsn, gemeente, success, timestamp, ttl, error);
+  await registerIssueEvent(bsn, gemeente, success, timestamp, error);
   await updateSessionStatus(session, bsn);
   return Response.json({ message: 'success' });
-
 }
 
 /**
  * Logs the issue event for collecting statistics one usage of the irma-issue-app
- * @param dynamoDBClient dynamodbclient
  * @param bsn hashed BSN from session
  * @param gemeente municipality from session
  * @param timestamp event time
- * @param ttl time to live in DynamoDB table
  * @param errorMessage optional error message from frontend
  */
 async function registerIssueEvent(
-  dynamoDBClient: DynamoDBClient,
   bsn: string,
   gemeente: string,
   success: boolean,
   timestamp: number,
-  ttl: string,
   errorMessage?: string,
 ) {
-
   // Combine with gemeente and key value to make more unique
   const diversify = `${bsn}/${gemeente}/${process.env.DIVERSIFYER}`;
   const subject = crypto.createHash('sha256').update(diversify).digest('hex');
@@ -74,23 +66,7 @@ async function registerIssueEvent(
     error = { error: { S: msg } };
   }
 
-  try {
-    console.log({ subject, timestamp, gemeente, success, error: error?.error.S }); // Also log to cloudwatch for testing
-    const log = new PutItemCommand({
-      Item: {
-        subject: { S: subject },
-        timestamp: { N: timestamp.toString() },
-        gemeente: { S: gemeente },
-        success: { BOOL: success },
-        ttl: { N: ttl },
-        ...error,
-      },
-      TableName: process.env.STATISTICS_TABLE,
-    });
-    await dynamoDBClient.send(log);
-  } catch (err) {
-    console.log('Could not add issue statistics', err);
-  }
+  console.log({ subject, timestamp, gemeente, success, error: error?.error.S });
 }
 
 /**
