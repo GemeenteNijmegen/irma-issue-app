f15a8eda3d7bf99d5e402c03fe2c4817296f4884
Marnix Dessing <m.dessing@nijmegen.nl>
2023-02-20 11:31:42 +0100

    feat: certificate validation with alternative names

--- src/UsEastCertificateStack.ts
+++ src/UsEastCertificateStack.ts
@@ -1,4 +1,11 @@
-import { aws_certificatemanager as CertificateManager, Stack, StackProps, aws_ssm as SSM } from 'aws-cdk-lib';
+import * as crypto from 'crypto';
+import {
+  aws_certificatemanager as CertificateManager,
+  Stack,
+  StackProps,
+  aws_ssm as SSM,
+  aws_route53 as route53,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Configurable, Configuration } from './Configuration';
 import { Statics } from './statics';
@@ -18,16 +25,36 @@ export class UsEastCertificateStack extends Stack {
     const hostedZone = importProjectHostedZone(this, configuration.deployToEnvironment.region);
     const subjectAlternativeNames = AppDomainUtil.getAlternativeDomainNames(configuration);
 
+    /**
+     * When using alternative domain names we cannot use auto-creation of the validation
+     * records in the hosted zone.
+     */
+    let validation = CertificateManager.CertificateValidation.fromDns(hostedZone);
+    if (subjectAlternativeNames) {
+      validation = CertificateManager.CertificateValidation.fromDns();
+    }
+
     const certificate = new CertificateManager.Certificate(this, 'certificate', {
       domainName: hostedZone.zoneName,
       subjectAlternativeNames,
-      validation: CertificateManager.CertificateValidation.fromDns(hostedZone),
+      validation: validation,
     });
 
     new SSM.StringParameter(this, 'cert-arn', {
       stringValue: certificate.certificateArn,
       parameterName: Statics.certificateArn,
     });
+
+    if (configuration.cnameRecords) {
+      Object.entries(configuration.cnameRecords).forEach(entry => {
+        const logicalId = crypto.createHash('md5').update(entry[0]).digest('hex').substring(0, 10);
+        new route53.CnameRecord(this, `record-${logicalId}`, {
+          zone: hostedZone,
+          recordName: entry[0],
+          domainName: entry[1],
+        });
+      });
+    }
 
   }
 }
