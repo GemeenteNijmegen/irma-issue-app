7b10517febf538e38b9370fbb42963c845ed03c2
Marnix Dessing <m.dessing@nijmegen.nl>
2024-05-22 11:42:12 +0200

    chore: calculate yearly statistics

--- src/lambdas/statistics/CalculateStatistics.lambda.ts
+++ src/lambdas/statistics/CalculateStatistics.lambda.ts
@@ -7,57 +7,65 @@ const logsClient = new CloudWatchLogsClient();
 
 export async function handler(event: ScheduledEvent) {
   console.log('Calculating statistics...');
+  const scope = event.detail?.scope;
+  const beginDate = event.detail?.beginDate;
+  const endDate = event.detail?.endDate ?? new Date();
+  console.log(event);
+
+  // Triggerd by EventBridge rule
+  if (!beginDate) {
+    await startCalculation(scope, new Date());
+    return;
+  }
+
+  console.log('Calculating for multiple days', beginDate, endDate);
+  const dates = getDates(scope, new Date(Date.parse(beginDate)), new Date(Date.parse(endDate)));
+  for (const date of dates) {
+    console.log('Calculating for date', date.toISOString());
+    await startCalculation(scope, date);
+  }
+}
+
+async function startCalculation(scope: string, date: Date) {
   try {
-    const scope = event.detail?.scope;
-    const beginDate = event.detail?.beginDate;
-    const endDate = event.detail?.endDate ?? new Date();
-    console.log(event);
-
-    if (beginDate) {
-      console.log('Calculating for multiple days', beginDate, endDate);
-      const dates = getDates(scope, new Date(Date.parse(beginDate)), new Date(Date.parse(endDate)));
-      for (const date of dates) {
-        console.log('Calculating for date', date.toISOString());
-        try {
-          if (scope == 'month') {
-            await calculateMonlthyStatistics(date);
-          } else {
-            await calculateDailyStatistics(date);
-          }
-        } catch (error) {
-          console.error(error);
-          console.log('Continuing with next data...');
-        }
-      };
-      return;
-    }
-
     if (scope == 'month') {
       console.log('Monthly calculations...');
-      await calculateMonlthyStatistics(new Date());
+      await calculateMonlthyStatistics(date);
+    } else if (scope == 'year') {
+      console.log('Monthly calculations...');
+      await calculateAnnualStatistics(date);
     } else {
       console.log('Dayly calculations...');
-      await calculateDailyStatistics(new Date());
+      await calculateDailyStatistics(date);
     }
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
   }
-};
+}
+
+async function calculateAnnualStatistics(date: Date) {
+  const start = new Date(date.getFullYear()-1, 0, 0, 0, 0, 0, 0); // Jan 1 last year
+  const end = new Date(date.getFullYear(), 0, 0, 0, 0, 0, 0); // Jan 1 this year
+  const dateStamp = start.toISOString().substring(0, 4); // Date stamp
+  console.log('Storing statistics for', dateStamp);
+  const count = await getStatistics(start.getTime(), end.getTime());
+  await storeInDynamodb(dateStamp, count, 'year');
+}
 
 
 async function calculateMonlthyStatistics(date: Date) {
-  const firstDay = new Date(date.getFullYear(), date.getMonth()-1, 1, 0, 0, 0, 0);
-  const lastDay = new Date(date.getFullYear(), date.getMonth(), 0, 0, 0, 0, 0);
-  const dateStamp = firstDay.toISOString().substring(0, 7); // Date stamp
+  const start = new Date(date.getFullYear(), date.getMonth()-1, 1, 0, 0, 0, 0); // First of last month
+  const end = new Date(date.getFullYear(), date.getMonth(), 0, 0, 0, 0, 0); // First of this month
+  const dateStamp = start.toISOString().substring(0, 7);
   console.log('Storing statistics for', dateStamp);
-  const count = await getStatistics(firstDay.getTime(), lastDay.getTime());
+  const count = await getStatistics(start.getTime(), end.getTime());
   await storeInDynamodb(dateStamp, count, 'month');
 }
 
 async function calculateDailyStatistics(date: Date) {
-  const start = new Date(date.getFullYear(), date.getMonth(), date.getDate()-1, 0, 0, 0, 0);
-  const end = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0);
-  const dateStamp = start.toISOString().substring(0, 10); // Date stamp
+  const start = new Date(date.getFullYear(), date.getMonth(), date.getDate()-1, 0, 0, 0, 0); // 00.00 yesterday
+  const end = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0); // 00.00 today
+  const dateStamp = start.toISOString().substring(0, 10);
   console.log('Storing statistics for', dateStamp);
   const count = await getStatistics(start.getTime(), end.getTime());
   await storeInDynamodb(dateStamp, count, 'day');
@@ -111,15 +119,17 @@ async function sleep(ms: number) {
   return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
-function getDates(stepsize: 'day'| 'month', startDate: Date, stopDate: Date): Date[] {
+function getDates(stepsize: 'day'| 'month' | 'year', startDate: Date, stopDate: Date): Date[] {
   var dateArray = new Array();
   var currentDate = startDate;
   while (currentDate <= stopDate) {
     dateArray.push(new Date (currentDate));
     if (stepsize=='day') {
       currentDate.setDate(currentDate.getDate() + 1);
-    } else {
+    } else if (stepsize=='month') {
       currentDate.setMonth(currentDate.getMonth() + 1);
+    } else {
+      currentDate.setFullYear(currentDate.getFullYear() + 1);
     }
   }
   return dateArray;
