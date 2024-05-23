7b7f31d6d7bfbf82ccd99154e9c968387a79e0a5
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-05-23 10:02:40 +0200

    feat: Add statistics as table
    The canvas-based chart is inaccessible to screen
    readers etc.. This adds the same data as a table,
    to provide a text-based alternative.

--- src/app/static-resources/static/scripts/statistics.js
+++ src/app/static-resources/static/scripts/statistics.js
@@ -26,6 +26,20 @@ const chart = new Chart(ctx, {
   },
 });
 
+const table = document.getElementById('statistics-table');
+
+function updateTable(data) {
+  let body = '<tbody>';
+  for(let dataPoint of Object.keys(data).reverse()) {
+    body += `<tr>
+      <td>${dataPoint}</td><td>${data[dataPoint]}</td>
+    </tr>`;
+  }
+  body += '</tbody>';
+  const newBody = htmlStringToElement(body);
+  const oldBody = table.querySelector('tbody');
+  oldBody.replaceWith(newBody);
+}
 
 // Plot data on page load
 plotData();
@@ -37,7 +51,20 @@ function plotData(){
   
   // Fetch data from the same lambda as renders this page
   fetch(window.location.href + "?scope="+scope).then(response => {
-    response.json().then(data => {
+      data = {
+        "2024-04": 17,
+        "2024-03": 8,
+        "2024-02": 5,
+        "2024-01": 5,
+        "2023-12": 3,
+        "2023-11": 1,
+        "2023-10": 9,
+        "2023-09": 7,
+        "2023-08": 6,
+        "2023-07": 13,
+        "2023-06": 1,
+        "2023-05": 3
+    }
       const labels = Object.keys(data).reverse();
       const values = Object.values(data).reverse();
       console.log(data);
@@ -48,9 +75,21 @@ function plotData(){
       });
       console.log('Updating chart...');
       chart.update();
+      updateTable(data);
     });
-  }).catch(error => {
-    console.error(error);
-  });
 
+}
+
+
+/**
+ * Transform an html-string to an actual nodetree
+ * 
+ * @param {string} html the valid HTML as as string
+ * @returns {Node} the html node
+ */
+function htmlStringToElement(html) {
+  var template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
 }
