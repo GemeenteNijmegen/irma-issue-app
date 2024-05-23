49fcae62df2cf30b3945b8a9729a25630292d786
Marnix Dessing <m.dessing@nijmegen.nl>
2024-05-23 15:08:00 +0200

    feat: improved tracability for statistics and session in container

--- src/app/issue/issueRequestHandler.ts
+++ src/app/issue/issueRequestHandler.ts
@@ -10,27 +10,43 @@ import { LogsUtil } from '../code/LogsUtil';
 import render from '../code/Render';
 import { YiviApi } from '../code/YiviApi';
 
-/**
- * Check login and handle request
- */
-export async function issueRequestHandler(
-  cookies: string,
-  brpApi: BrpApi,
-  yiviApi: YiviApi,
-  dynamoDBClient: DynamoDBClient,
-  logsClient: CloudWatchLogsClient,
-) {
-
-  let session = new Session(cookies, dynamoDBClient);
-  await session.init();
-  if (session.isLoggedIn() == true) {
-    return handleLoggedinRequest(session, brpApi, yiviApi, logsClient);
-  }
-  console.info('Redirecting to login...');
-  return Response.redirect('/login');
+export interface Params {
+  cookies: string;
+  requestId: string;
 }
 
-/**
+export interface IssueRequestHandlerProps {
+  dynamoDBClient: DynamoDBClient;
+  logsClient: CloudWatchLogsClient;
+  brpApi: BrpApi;
+  yiviApi: YiviApi;
+}
+
+export class IssueRequestHandler {
+  private readonly dynamoDBClient: DynamoDBClient;
+  private readonly logsClient: CloudWatchLogsClient;
+  private readonly brpApi: BrpApi;
+  private readonly yiviApi: YiviApi;
+
+  constructor(props: IssueRequestHandlerProps) {
+    this.dynamoDBClient = props.dynamoDBClient;
+    this.logsClient = props.logsClient;
+    this.brpApi = props.brpApi;
+    this.yiviApi = props.yiviApi;
+  }
+
+  async handle(params: Params) {
+    const session = new Session(params.cookies, this.dynamoDBClient);
+    await session.init();
+    if (session.isLoggedIn() == true) {
+      return this.handleLoggedinRequest(session, params.requestId);
+    }
+    console.info('Redirecting to login...');
+    return Response.redirect('/login');
+  }
+
+
+  /**
  * Request persoonsgegevens form BRP send them to the YIVI server
  * logs the issue event and passes the yivi sessionPtr to the render.
  * @param session
@@ -38,102 +54,117 @@ export async function issueRequestHandler(
  * @param yiviApi
  * @returns
  */
-async function handleLoggedinRequest(session: Session, brpApi: BrpApi, yiviApi: YiviApi, logsClient: CloudWatchLogsClient) {
-  let error = undefined;
+  async handleLoggedinRequest(session: Session, requestId: string) {
+    let error = undefined;
 
-  // BRP request
-  let naam = undefined;
-  let brpData = undefined;
-  if (!error) {
-    const bsn = session.getValue('bsn');
-    brpData = await brpApi.getBrpData(bsn);
-    await LogsUtil.logToCloudWatch(logsClient, 'TICK: BRP', process.env.TICKEN_LOG_GROUP_NAME, process.env.TICKEN_LOG_STREAM_NAME);
-    naam = brpData?.Persoon?.Persoonsgegevens?.Naam;
-    if (brpData.error || !naam) {
-      error = 'Het ophalen van uw persoonsgegevens is mis gegaan. Probeer het later opnieuw.';
-      if (brpData.error?.includes('duurt te lang')) {
-        error = 'Het ophalen van uw persoonsgegevens duurde te lang. Probeer het later opnieuw.';
+    // BRP request
+    let naam = undefined;
+    let brpData = undefined;
+    if (!error) {
+      const bsn = session.getValue('bsn');
+      brpData = await this.brpApi.getBrpData(bsn);
+      await LogsUtil.logToCloudWatch(this.logsClient, 'TICK: BRP', process.env.TICKEN_LOG_GROUP_NAME, process.env.TICKEN_LOG_STREAM_NAME);
+      naam = brpData?.Persoon?.Persoonsgegevens?.Naam;
+      if (brpData.error || !naam) {
+        error = 'Het ophalen van uw persoonsgegevens is mis gegaan. Probeer het later opnieuw.';
+        if (brpData.error?.includes('duurt te lang')) {
+          error = 'Het ophalen van uw persoonsgegevens duurde te lang. Probeer het later opnieuw.';
+        }
       }
     }
+
+    // Start YIVI session
+    let yiviFullSessionEncoded = undefined;
+    let yiviSessionToken = undefined;
+    if (!error) {
+      const loa = session.getValue('loa');
+      const yiviResponse = await this.yiviApi.startSession(brpData, loa);
+      if (!yiviResponse.error) {
+        yiviSessionToken = yiviResponse?.token;
+        yiviFullSessionEncoded = Buffer.from(JSON.stringify(yiviResponse), 'utf-8').toString('base64');
+      } else {
+        error = 'Er is iets mis gegaan bij het inladen van uw persoonsgegevens in Yivi. Probeer het later opnieuw.';
+      }
+    }
+
+    await this.storeIssueEventInSession(session);
+    await this.logIssueEvent(session, brpData, requestId, yiviSessionToken, error);
+
+    // Render the page
+    const data = {
+      title: 'opladen',
+      yiviServer: `https://${this.yiviApi.getHost()}`,
+      error: error,
+      yiviFullSession: yiviFullSessionEncoded,
+    };
+    const html = await render(data, template.default);
+    return Response.html(html, 200, session.getCookie());
   }
 
-  // Start YIVI session
-  let yiviFullSession = undefined;
-  if (!error) {
+  /**
+   * Add the requred issue event data to the sessino for
+   * collecting statistics one usage of the yivi-issue-app later on
+   * @param session the uses session to store data in
+   */
+  async storeIssueEventInSession(session: Session) {
+    const loggedin = session.getValue('loggedin', 'BOOL') ?? false;
     const loa = session.getValue('loa');
-    const yiviResponse = await yiviApi.startSession(brpData, loa);
-    if (!yiviResponse.error) {
-      yiviFullSession = Buffer.from(JSON.stringify(yiviResponse), 'utf-8').toString('base64');
-    } else {
-      error = 'Er is iets mis gegaan bij het inladen van uw persoonsgegevens in Yivi. Probeer het later opnieuw.';
+    const bsn = session.getValue('bsn');
+    let issueAttempt = session.getValue('issueAttempt', 'N') ?? '0';
+    const incrementedIssueAttempt = parseInt(issueAttempt) + 1;
+
+    try {
+      await session.updateSession({
+        loggedin: { BOOL: loggedin },
+        bsn: { S: bsn },
+        loa: { S: loa },
+        issueAttempt: { N: incrementedIssueAttempt.toString() },
+      });
+    } catch (err) {
+      console.log('Could not add issue statistics to session', err);
     }
   }
 
-  await storeIssueEventInSession(session);
-  await logIssueEvent(logsClient, session, brpData, error);
+  /**
+   * Log the issue event to a separate log group
+   * @param client
+   * @param session
+   * @param brpData
+   * @param error
+   */
+  async logIssueEvent(session: Session, brpData: any, requestId: string, yiviSessionToken: string, error?: string) {
 
-  // Render the page
-  const data = {
-    title: 'opladen',
-    yiviServer: `https://${yiviApi.getHost()}`,
-    error: error,
-    yiviFullSession: yiviFullSession,
-  };
-  const html = await render(data, template.default);
-  return Response.html(html, 200, session.getCookie());
-}
+    // Setup statistics data
+    const loa = loaToString(session.getValue('loa'));
+    const issueAttempt = session.getValue('issueAttempt', 'N') ?? 0;
+    const gemeente = brpData?.Persoon?.Adres?.Gemeente;
+    const timestamp = Date.now();
 
-/**
- * Add the requred issue event data to the sessino for
- * collecting statistics one usage of the yivi-issue-app later on
- * @param session the uses session to store data in
- */
-async function storeIssueEventInSession(session: Session) {
-  const loggedin = session.getValue('loggedin', 'BOOL') ?? false;
-  const loa = session.getValue('loa');
-  const bsn = session.getValue('bsn');
-  let issueAttempt = session.getValue('issueAttempt', 'N') ?? '0';
-  const incrementedIssueAttempt = parseInt(issueAttempt) + 1;
+    // Construct the subject and make it as specific as possible so it cannot be bruteforced
+    const brpDataAsJson = JSON.stringify(brpData ?? { unknown: 'faild to get brp data' });
+    const diversify = `${brpDataAsJson}/${process.env.DIVERSIFYER}`;
+    const subject = crypto.createHash('sha256').update(diversify).digest('hex');
 
-  try {
-    await session.updateSession({
-      loggedin: { BOOL: loggedin },
-      bsn: { S: bsn },
-      loa: { S: loa },
-      issueAttempt: { N: incrementedIssueAttempt.toString() },
-    });
-  } catch (err) {
-    console.log('Could not add issue statistics to session', err);
-  }
-}
+    // For tracability between webapp and server we hash the session token to correlate with
+    // the session token that is logged by the irmago server.
+    let yiviSessionFingerprint = 'no session token';
+    if (yiviSessionToken) {
+      yiviSessionFingerprint = crypto.createHash('sha256').update(yiviSessionToken).digest('hex');
+    }
 
-/**
- * Log the issue event to a separate log group
- * @param client
- * @param session
- * @param brpData
- * @param error
- */
-async function logIssueEvent(client: CloudWatchLogsClient, session: Session, brpData: any, error?: string) {
+    // Constuct the message
+    let message = JSON.stringify({ timestamp, gemeente, subject, loa, issueAttempt, requestId, yiviSessionFingerprint });
+    if (error) {
+      message = JSON.stringify({ timestamp, loa, issueAttempt, error, requestId, yiviSessionFingerprint });
+    }
 
-  // Setup statistics data
-  const loa = loaToString(session.getValue('loa'));
-  const issueAttempt = session.getValue('issueAttempt', 'N') ?? 0;
-  const gemeente = brpData?.Persoon?.Adres?.Gemeente;
-  const timestamp = Date.now();
+    // Log the message
+    const group = process.env.STATISTICS_LOG_GROUP_NAME;
+    const stream = process.env.STATISTICS_LOG_STREAM_NAME;
+    await LogsUtil.logToCloudWatch(this.logsClient, message, group, stream);
 
-  // Construct the subject and make it as specific as possible so it cannot be bruteforced
-  const brpDataAsJson = JSON.stringify(brpData ?? { unknown: 'faild to get brp data' });
-  const diversify = `${brpDataAsJson}/${process.env.DIVERSIFYER}`;
-  const subject = crypto.createHash('sha256').update(diversify).digest('hex');
-
-  let message = JSON.stringify({ timestamp, gemeente, subject, loa, issueAttempt });
-  if (error) {
-    message = JSON.stringify({ timestamp, loa, issueAttempt, error });
   }
 
-  const group = process.env.STATISTICS_LOG_GROUP_NAME;
-  const stream = process.env.STATISTICS_LOG_STREAM_NAME;
-  await LogsUtil.logToCloudWatch(client, message, group, stream);
-
 }
+
+
